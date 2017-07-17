25c4029a8c140aeb6b04bb63839612b19a808cd6
nemu-ism <vanilla_chip_s@yahoo.co.jp>
2017-07-17 17:55:42 +0900

    first_commit

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :twitter, 'QtoIuqnXzNvB5fj8Su2tASXaV', 'B9zL5AtlWNifA2NC1nRjhJHCKLXja54FNJHXrieSHUwrEE2a1s'
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'homes/top'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
+  root 'homes#top'
+end
