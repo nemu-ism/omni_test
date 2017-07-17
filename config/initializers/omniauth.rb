84bafa88ae6663c3fcea97b9ba5fecac26e4043d
nemu-ism <vanilla_chip_s@yahoo.co.jp>
2017-07-17 20:06:39 +0900

    キー類をシークレット化

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'QtoIuqnXzNvB5fj8Su2tASXaV', 'B9zL5AtlWNifA2NC1nRjhJHCKLXja54FNJHXrieSHUwrEE2a1s'
+  provider :twitter, Rails.application.secrets.twitter_consumer_key, Rails.application.secrets.twitter_consumer_secret
 end
